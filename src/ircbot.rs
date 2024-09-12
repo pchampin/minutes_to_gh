e8856a315055c036bd37d070b8d225c1022812bb
Pierre-Antoine Champin <pierre-antoine@w3.org>
2024-09-12 14:29:03 +0200

    add IRC throttling
    now the bot will not send more than one message per second on each channel

--- src/ircbot.rs
+++ src/ircbot.rs
@@ -1,9 +1,10 @@
 use anyhow::Result;
 use futures::prelude::*;
+use governor::{DefaultKeyedRateLimiter, Quota, RateLimiter};
 use irc::client::prelude::*;
 use regex::{Regex, RegexBuilder};
 
-use std::sync::LazyLock;
+use std::{sync::LazyLock, time::Duration};
 
 use crate::{
     args::{EngineArgs, FinitePositiveF64, IrcBotArgs},
@@ -22,6 +23,7 @@ pub async fn command(token: String, args: IrcBotArgs) -> Result<()> {
 struct Bot {
     client: Client,
     token: String,
+    governor: DefaultKeyedRateLimiter<String>,
 }
 
 impl Bot {
@@ -31,31 +33,44 @@ impl Bot {
         // identify comes from ClientExt
         client.identify()?;
         log::info!("Identified as {}", client.current_nickname());
-        Ok(Self { client, token })
+        let governor =
+            RateLimiter::keyed(Quota::with_period(Duration::from_secs_f64(1.0)).unwrap());
+        Ok(Self {
+            client,
+            token,
+            governor,
+        })
     }
 
     async fn poll(&mut self) -> Result<()> {
+        // the spawn below ensures that messages are sent as soon as client.send_X is called,
+        // rather than on the next poll to the client.stream
+        tokio::spawn(self.client.outgoing().unwrap());
         let mut stream = self.client.stream()?;
         while let Some(message) = stream.next().await.transpose()? {
             match &message.command {
-                Command::INVITE(_, channel) => match self.client.send_join(channel) {
-                    Ok(_) => log::info!("joining {channel} after being invited"),
-                    Err(err) => log::error!("IRC error: {err:?}"),
-                },
+                Command::INVITE(_, channel) => {
+                    self.governor.until_key_ready(channel).await;
+                    match self.client.send_join(channel) {
+                        Ok(_) => log::info!("joining {channel} after being invited"),
+                        Err(err) => log::error!("IRC error: {err:?}"),
+                    }
+                }
                 Command::PRIVMSG(channel, content) => {
                     if let Some(cmd_str) = self.for_me(content) {
                         let cmd = BotCommand::from(cmd_str);
                         log::debug!("on {channel} got {cmd:?}, parsed from {cmd_str:?}");
                         let res = match cmd {
-                            BotCommand::Bye => self.bye(channel),
-                            BotCommand::Help => self.help(&message),
+                            BotCommand::Bye => self.bye(channel).await,
+                            BotCommand::Help => self.help(&message).await,
                             BotCommand::LinkIssues => self.link_issues(&message).await,
                             BotCommand::Debug => self.debug(&message).await,
-                            BotCommand::Unrecognized => self.unrecognized(&message, cmd_str),
+                            BotCommand::Unrecognized => self.unrecognized(&message, cmd_str).await,
                         };
                         if let Err(err) = res {
                             log::error!("Error: {err:?}");
                             self.respond(&message, &format!("Something wrong happened: {err}"))
+                                .await
                                 .unwrap_or(());
                         }
                     }
@@ -83,14 +98,15 @@ impl Bot {
         }
     }
 
-    fn bye(&self, channel: &str) -> Result<()> {
+    async fn bye(&self, channel: &String) -> Result<()> {
+        self.governor.until_key_ready(channel).await;
         if channel.is_channel_name() {
             self.client.send_part(channel)?;
         }
         Ok(())
     }
 
-    fn help(&self, message: &Message) -> Result<()> {
+    async fn help(&self, message: &Message) -> Result<()> {
         debug_assert!(matches!(message.command, Command::PRIVMSG(..)));
 
         let nickname = message.source_nickname().unwrap_or("people");
@@ -98,7 +114,8 @@ impl Bot {
         self.respond(
             message,
             &format!("{nickname}, I am {}.", env!("CARGO_PKG_DESCRIPTION"),),
-        )?;
+        )
+        .await?;
         self.respond(
             message,
             &format!(
@@ -106,11 +123,13 @@ impl Bot {
                 env!("CARGO_PKG_NAME"),
                 env!("CARGO_PKG_VERSION"),
             ),
-        )?;
+        )
+        .await?;
         self.respond(
             message,
             &format!("... To know more, see {}", env!("CARGO_PKG_HOMEPAGE")),
         )
+        .await
     }
 
     async fn link_issues(&self, message: &Message) -> Result<()> {
@@ -162,20 +181,25 @@ impl Bot {
                 match outcome.kind {
                     Created(comment) => {
                         self.respond(message, &format!("comment created: {comment}"))
+                            .await
                     }
-                    Faked => self.respond(
-                        message,
-                        &format!("comment would have been created for: {issue}"),
-                    ),
+                    Faked => {
+                        self.respond(
+                            message,
+                            &format!("comment would have been created for: {issue}"),
+                        )
+                        .await
+                    }
                     Skipped(comment) => {
                         self.respond(message, &format!("comment already there: {comment}"))
+                            .await
                     }
                 }
             })
             .await
     }
 
-    fn unrecognized(&self, message: &Message, cmd_str: &str) -> Result<()> {
+    async fn unrecognized(&self, message: &Message, cmd_str: &str) -> Result<()> {
         debug_assert!(matches!(message.command, Command::PRIVMSG(..)));
 
         let nickname = message.source_nickname().unwrap_or("people");
@@ -183,16 +207,18 @@ impl Bot {
             message,
             &format!("sorry {nickname}, I don't understand {cmd_str:?}"),
         )
+        .await
     }
 
-    fn respond(&self, message: &Message, response: &str) -> Result<()> {
+    async fn respond(&self, message: &Message, response: &str) -> Result<()> {
         debug_assert!(matches!(message.command, Command::PRIVMSG(..)));
 
-        let Command::PRIVMSG(_, msg_str) = &message.command else {
+        let Command::PRIVMSG(target, msg_str) = &message.command else {
             unreachable!();
         };
         let action = msg_str.starts_with("\u{1}ACTION ");
-        let target = message.response_target().unwrap();
+        let target = my_response_target(target, message).unwrap();
+        self.governor.until_key_ready(target).await;
         if action {
             self.client.send_action(target, response)?;
         } else {
@@ -241,5 +267,17 @@ impl From<&'_ str> for BotCommand {
         } else {
             Unrecognized
         }
+    }
+}
+
+/// Version of Message:response_target that returns &Strings instead of &str,
+/// so that we can pass it as keys to Bot::governor
+fn my_response_target<'a>(target: &'a String, msg: &'a Message) -> Option<&'a String> {
+    if target.is_channel_name() {
+        Some(target)
+    } else if let Prefix::Nickname(name, _, _) = msg.prefix.as_ref()? {
+        Some(name)
+    } else {
+        None
     }
 }
